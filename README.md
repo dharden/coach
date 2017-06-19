bd4637ed57e084cf29743a09d651d786a6ec0fe9
Daniel Harden <dharden@zappos.com>
2017-06-19 12:47:56 -0700

    let's get ready to rumble

---
+++ README.md
@@ -0,0 +1,18 @@
+# Coach
+![hello i am the coach](./coach.jpg "let's get ready to rumble")
+_let's get ready to rumble_
+
+## Usage
+1. Get yourself [a slack webhook](https://api.slack.com/incoming-webhooks)
+2. `./coach.sh https://hook.slack.com/whateveryourwebhookurlis [channel username text emoji]`
+3. ???
+4. 💪💪💪💪💪💪
+
+## EHOH
+You might wanna put this in a crontab, to run every hour, on the hour. Here's some examples:
+### Every Hour On the Hour, no exceptions
+`0 * * * * /path/to/coach.sh`
+### Every Hour On the Hour, reasonable times
+`0 10-11,13-16 * * * /path/to/coach.sh`
+### Every Hour On the Hour, but like, your server in on UTC, and you're in PDT
+`0 17-18,20-23 * * * /path/to/coach.sh`
---
+++ coach.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#                   ___
+#    .--. _       .'   Y.       _ .--.
+#   '._  ' )    _|  _.'  |     ( '  _.'
+#      '. \   .' |-'  O O|      / .'        Good jorb!
+#        \ \ '.--'`.___.'      / /
+#         \ \_______| |_______/ /
+#          \________   ________/
+#                  |   |
+#                  |___|
+#                 .'___`.
+#                |   .'  |
+#                |  /__  |
+#                 `.___.'
+#                 |     |
+#                 |     |
+#                 |     |
+#                 |     |
+#                 |  __ |
+#            ___  |(   )|  ___
+#          .'   ''  ) (  ''   '.
+#         '..__..--'   '--..__..'
+
+if [ "$#" -eq 0 ]
+then
+  echo -e "slow down champ, you have to give me a webhook url, and some other stuff if you want \n"
+  echo -e "Usage: ./coach.sh https://hook.slack.com/whateveryourwebhookurlis [channel username text emoji] \n"
+  echo -e "let's get strong 💪 "
+  exit 1
+fi
+
+CHANNEL=${2:-#ehoh}
+USERNAME=${3:-coach}
+TEXT=${4:-<!here> lets get ready to rumble}
+EMOJI=${5:-:muscle:}
+curl -X POST --data-urlencode 'payload={"channel": "'$CHANNEL'", "username": "'$USERNAME'", "text": "'$TEXT'", "icon_emoji": "'$EMOJI'"}' $1
